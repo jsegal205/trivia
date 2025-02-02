baf137e222e8bf6cc5f4652a64c521389ada5df9
Jim Segal <hello@jimsegal.com>
2025-02-02 14:23:46 -0600

    add some chakra defaults

--- assets/src/routes/__root.tsx
+++ assets/src/routes/__root.tsx
@@ -1,13 +1,19 @@
+import { Flex } from "@chakra-ui/react";
 import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 export const Route = createRootRoute({
   component: () => (
-    <>
-      <div>
+    <Flex
+      as="main"
+      direction="column"
+      justifyContent="space-between"
+      minHeight="100vh"
+    >
+      <Flex as="nav" margin="4">
         <Link to="/">Home</Link>
-      </div>
+      </Flex>
       <Outlet />
       <TanStackRouterDevtools />
-    </>
+    </Flex>
   ),
 });
--- assets/src/routes/index.tsx
+++ assets/src/routes/index.tsx
@@ -1,5 +1,14 @@
+import { Flex, Heading } from "@chakra-ui/react";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
-  component: () => <div>Hello home!</div>,
+  component: () => <LandingPage />,
 });
+
+const LandingPage = () => (
+  <Flex direction="column" alignItems="center">
+    <Heading as="h1" fontSize="6xl">
+      Welcome to Trivia!
+    </Heading>
+  </Flex>
+);
