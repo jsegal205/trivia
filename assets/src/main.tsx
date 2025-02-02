0597f1430b10fe407bc63c3ad46f3fa7fdc612f5
Jim Segal <hello@jimsegal.com>
2025-02-02 12:28:30 -0600

    add typescript

---
+++ assets/src/main.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App: React.FC = () => <main>Helllooo react</main>;
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <App />
+);
--- assets/vite.config.js
+++ assets/vite.config.js
@@ -4,4 +4,5 @@ import react from "@vitejs/plugin-react";
 export default {
   server: { port: 5173 },
   plugins: [react()],
+  resolve: [".tsx", ".ts", ".jsx", ".js"],
 };
