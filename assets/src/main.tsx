ce47c6aa3cadc53361b79c4bc5d874ab2ac56842
Jim Segal <hello@jimsegal.com>
2025-02-02 14:05:22 -0600

    add chakra provider, change index from lazy

--- assets/src/main.tsx
+++ assets/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
+import { Provider } from "@/components/ui/provider"
 
 const router = createRouter({ routeTree });
 declare module "@tanstack/react-router" {
@@ -14,7 +15,9 @@ if (!rootElement.innerHTML) {
   const root = createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <Provider>
+        <RouterProvider router={router} />
+      </Provider>
     </StrictMode>
   );
 }
