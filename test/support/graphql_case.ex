740f63577a08ca3886eb0393ef767fefc512fa42
Jim Segal <hello@jimsegal.com>
2025-02-02 13:14:13 -0600

    add schema test

---
+++ test/support/graphql_case.ex
@@ -0,0 +1,22 @@
+defmodule TriviaWeb.GraphQLCase do
+  use ExUnit.CaseTemplate
+
+  using do
+    quote do
+      use TriviaWeb.ConnCase
+      import Phoenix.ConnTest
+      import Trivia.Factory
+
+      alias TriviaWeb.Router.Helpers, as: Routes
+
+      @endpoint TriviaWeb.Endpoint
+
+      def graphql_request(conn, query, variables \\ %{}) do
+        post(conn, "/graphql", %{
+          "query" => query,
+          "variables" => variables
+        })
+      end
+    end
+  end
+end
