dd272cdb55c34abe99fb634381d9399070d78e50
Jim Segal <hello@jimsegal.com>
2025-02-02 13:51:11 -0600

    add actual files

---
+++ assets/src/routes/__root.tsx
@@ -0,0 +1,13 @@
+import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <div>
+        <Link to="/">Home</Link>
+      </div>
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  ),
+});
